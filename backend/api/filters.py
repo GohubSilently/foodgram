90fa9f42a8bbffa3e49518a441462f49a57e5567
Vadim <khalinvadim3@gmail.com>
2025-10-30 12:07:32 +0300

    Change the options to filter ingredients

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -4,7 +4,11 @@ from .models import Ingredient
 
 
 class IngredientFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='startswith')
+    name = django_filters.CharFilter()
     class Meta:
         model = Ingredient
-        fields = ('name',)
+        fields = {
+            'name': ('istartswith', 'icontains'),
+        }
+        # TODO: Если не будет работать фильтрация, тогда надо переписать этот код на более низком уровне
+        #  сейчас фильтрация работает по методу name__istartwith, а не просто по поиску.
